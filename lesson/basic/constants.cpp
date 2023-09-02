13126793aef852f751e3b4ebcfe501975bdf6fba
zonaetmunna <zonaetmonna@gmail.com>
2023-09-02 17:15:45 +0600

    basic

---
+++ lesson/basic/constants.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    const double PI = 3.14159;
+    const int MAX_VALUE = 100;
+
+    std::cout << "PI: " << PI << std::endl;
+    std::cout << "MAX_VALUE: " << MAX_VALUE << std::endl;
+
+    return 0;
+}
---
+++ lesson/basic/type_conversion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int num1 = 10;
+    double num2 = 5.5;
+
+    double sum = num1 + num2;
+
+    std::cout << "Sum: " << sum << std::endl;
+
+    return 0;
+}
