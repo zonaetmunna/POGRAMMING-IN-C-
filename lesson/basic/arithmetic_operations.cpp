360af6a784b43b54f1ce523e7fd193738cc8d51c
zonaetmunna <zonaetmonna@gmail.com>
2023-09-02 17:05:10 +0600

    basic

---
+++ lesson/basic/arithmetic_operations.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int num1 = 10, num2 = 5;
+    int sum = num1 + num2;
+    int difference = num1 - num2;
+    int product = num1 * num2;
+    int quotient = num1 / num2;
+
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Difference: " << difference << std::endl;
+    std::cout << "Product: " << product << std::endl;
+    std::cout << "Quotient: " << quotient << std::endl;
+
+    return 0;
+}
---
+++ lesson/basic/variables_and_constants.c++
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+int main() {
+    int age = 25;
+    const double PI = 3.14159;
+
+    std::cout << "Age: " << age << std::endl;
+    std::cout << "PI: " << PI << std::endl;
+
+    return 0;
+}
+
