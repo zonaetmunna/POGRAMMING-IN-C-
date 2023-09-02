4f1423c1850fbf057a06b3b170a46e618e100068
zonaetmunna <zonaetmonna@gmail.com>
2023-09-02 17:06:54 +0600

    basic

---
+++ lesson/basic/basic_data_types.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int age = 25;
+    double weight = 65.5;
+    char grade = 'A';
+
+    std::cout << "Age: " << age << std::endl;
+    std::cout << "Weight: " << weight << std::endl;
+    std::cout << "Grade: " << grade << std::endl;
+
+    return 0;
+}
